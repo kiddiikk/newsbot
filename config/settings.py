0d8e8db14059742df7ec014443159288a1cc50d6
kiddiikk <avitoavito1810@gmail.com>
2026-09-26 05:03:08 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -7,6 +7,7 @@ DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///bot.db")
 ADMIN_IDS = [int(x) for x in os.getenv("ADMIN_IDS", "").split(",") if x]
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 GRAMKIT_DATABASE_URL = os.getenv("GRAMKIT_DATABASE_URL", DATABASE_URL)
+CHANNEL_ID = os.getenv("CHANNEL_ID")
 
 RSS_CHECK_INTERVAL = 3600
 MAX_CHANNELS_PER_USER = 5
